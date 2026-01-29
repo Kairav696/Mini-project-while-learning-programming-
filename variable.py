22c358f97966881e4528377399e64a7205d01e18
Kairav696 <kairavsinghm@gmail.com>
2026-01-29 12:28:43 +0530

    Variable topic explanation

---
+++ variable.py
@@ -0,0 +1,81 @@
+#STRING
+
+first_name = "bro"
+
+print(f"hello {first_name}")
+food = "pizza"
+
+email = "kairavsinghm@.com"
+
+print(f"your email is:{email}")
+
+
+print(f"you like {food}")
+
+
+
+
+# integers 
+
+
+age = "18"
+quantity = 69
+
+num_of_students = 1000
+
+print(f"your class has {num_of_students} students ")
+
+print(f"you are buying {quantity} items ")
+print(f"you are {age} years old")
+
+
+#FLOAt 
+
+price = 1000000000
+gpa = 4.0
+
+print(f"your gpa is: {gpa}")
+print(f"the price is ${price}")
+
+#BOOLEAN
+
+is_student = False
+
+for_sale = True
+
+
+if for_sale:
+          print("that item is for sale")
+else:
+     print("that item is not available")
+ 
+ 
+if is_student:
+    print("you are a student")
+else: 
+    print("you are not a student")    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
